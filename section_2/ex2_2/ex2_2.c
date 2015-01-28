aafc4583c9b19eac000cff3d5e594641055e3ae0
fordea <fordea90@gmail.com>
2015-01-28 19:50:17 +0000

    Completed task 2.2

---
+++ section_2/ex2_2/ex2_2.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main (void)
+{
+	printf("Parkinson's Law:\nWork expands so as to ");
+	printf("fill the time\n");
+	printf("available for its completion.\n");
+	return 0;
+}
