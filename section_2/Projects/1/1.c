fe7a4e3b7571dfdab9a69c9bd4627da9cd99107d
Alex Forde <fordea90@gmail.com>
2015-02-15 14:13:43 +0000

    completed s2/p1

---
+++ section_2/Projects/1/1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void)
+{
+		printf("        *\n");
+		printf("       *\n");
+		printf("      *\n");
+		printf("*    *\n");
+		printf(" *  *\n");
+		printf("  *\n");
+}
