62cd7166fb23ea91b55ef9e0f8522fb3f71f27e9
Alex Forde <fordea90@gmail.com>
2015-02-15 16:36:50 +0000

    Completed s2/p5

---
+++ section_2/Projects/5/5.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int x;
+
+int main(void)
+{
+    printf("Enter a value: ");
+    scanf("%d", &x);
+    printf("%d", (3 * (x * x * x * x * x)) + (2 * (x * x * x * x)) + (5 * (x * x * x)) + (x * x) + (7 * x) - 6);
+
+    return 0;
+}
