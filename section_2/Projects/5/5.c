09de0af4ddd1e624f46a9be1e9dbaf9e43a8acef
Alex Forde <fordea90@gmail.com>
2015-02-15 17:02:52 +0000

    Refactored s2/p5 to increase clarity

--- section_2/Projects/5/5.c
+++ section_2/Projects/5/5.c
@@ -6,7 +6,12 @@ int main(void)
 {
     printf("Enter a value: ");
     scanf("%d", &x);
-    printf("%d", (3 * (x * x * x * x * x)) + (2 * (x * x * x * x)) + (5 * (x * x * x)) + (x * x) + (7 * x) - 6);
+    printf("%d", (3 * (x * x * x * x * x))
+                 + (2 * (x * x * x * x))
+                 + (5 * (x * x * x))
+                 + (x * x)
+                 - (7 * x)
+                 - 6);
 
     return 0;
 }
