01c592cdc0bc2078f0995068f9e2bf8e911a658d
Alex Forde <fordea90@gmail.com>
2015-02-15 14:11:37 +0000

    fixed tab spacing in s2/e3

--- section_2/Exercises/3/3.c
+++ section_2/Exercises/3/3.c
@@ -4,7 +4,7 @@
 
 int main(void)
 {
-	int height = 8, length = 12, width = 10, volume = height * length * 		width;
+	int height = 8, length = 12, width = 10, volume = height * length * width;
 
 	printf("Dimensions: %dx%dx%d\n", length, width, height);
 	printf("Volume (cubic inches): %d\n", volume);
