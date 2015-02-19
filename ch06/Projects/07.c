d6ba946ce7f932a1d88e39d82201b06aa28dbf02
Alex Forde <fordea90@gmail.com>
2015-02-19 13:21:48 +0000

    Completed Project 7 in Chapter 6.

---
+++ ch06/Projects/07.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int i, n, odd, square;
+
+    printf("This program prints a table of squres.\n");
+    printf("Enter a number of entries in table: ");
+    scanf("%d", &n);
+
+    for (i = 1, odd = 3, square = 1; i <= n; i++, square += odd, odd += 2)
+        printf("%10d%10d\n", i, square);
+
+    return 0;
+}
+
