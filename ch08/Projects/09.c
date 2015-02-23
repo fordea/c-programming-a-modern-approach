332b5c8584c0395c6fb49e1d0854f33eba64ff0f
Alex Forde <fordea90@gmail.com>
2015-02-23 15:37:43 +0000

    Amended Project 9 in Chapter 8, should be more readable in this form.

--- ch08/Projects/09.c
+++ ch08/Projects/09.c
@@ -1,72 +1,98 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <stdbool.h>
 
-#define ROWS 10
-#define COLS 10
-#define EMPTY '.'
+#define NUM_ROWS 10
+#define NUM_COLS 10
+#define EMPTY_CELL '.'
+
+#define LEFT 0
+#define DOWN 1
+#define UP 2
+#define RIGHT 3
+
 int main(void)
 {
-    char grid[ROWS][COLS] = {}, letter;
-    int row, col;
-    typedef int bool;
-    bool performed_move;
+    char grid[NUM_ROWS][NUM_COLS] = {}, letter;
+    int i, row, col, potential_moves_count, potential_move;
+    bool potential_moves[4], move_performed;
 
     srand((unsigned) time(NULL));
 
     /* populate the grid with '.' */
-    for (row = 0; row < ROWS; row++) {
-        for (col = 0; col < COLS; col++) {
+    for (row = 0; row < NUM_ROWS; row++) {
+        for (col = 0; col < NUM_COLS; col++) {
             grid[row][col] = '.';
         }
     }
 
-    /* Perform alphabet trail through the grid */
+    /* Perform alphabet trail through grid cells */
     row = 0; col = 0;
     for (letter = 'A'; letter <= 'Z'; letter++) {
 
-        grid[row][col] = (char) letter;
+        grid[row][col] = letter;
 
-        /* Terminate if no more moves possible  */
-        if (  (!(col-1 >= 0 && grid[row][col-1] == EMPTY))   //left not possible
-           && (!(row+1 < ROWS && grid[row+1][col] == EMPTY)) //down not possible
-           && (!(row-1 >= 0 && grid[row-1][col] == EMPTY))   //up not possible
-           && (!(col+1 < COLS && grid[row][col+1] == EMPTY)) //right not possible 
-           ) { printf("No moves possible\n"); break; }
+        /* Reset potential_moves array */
+        for (i = 0; i <= 3; potential_moves[i] = 0, i++)
 
-        /* At least one move must be possible, so find random move */
-        performed_move = 0;
-        for (;;) {
+        /* Find all potential moves from current cell */
+        potential_moves_count = 0;
+        if (col-1 >= 0 && grid[row][col-1] == EMPTY_CELL) {
+            potential_moves[LEFT] = true;
+            potential_moves_count++;
+        }
+        if (row+1 < NUM_ROWS && grid[row+1][col] == EMPTY_CELL) {
+            potential_moves[DOWN] = true;
+            potential_moves_count++;
+        }
+        if (row-1 >= 0 && grid[row-1][col] == EMPTY_CELL) {
+            potential_moves[UP] = true;
+            potential_moves_count++;
+        }
+        if (col+1 < NUM_COLS && grid[row][col+1] == EMPTY_CELL) {
+            potential_moves[RIGHT] = true;
+            potential_moves_count++;
+        }
 
-            switch (rand() % 4) {
-                case 0: // Left
-                    if (col - 1 >= 0 && grid[row][col-1] == EMPTY) {
+        /* Break if no moves are possible from current cell */
+        if (potential_moves_count == 0)
+            break;
+
+        /* Generate random moves until one matches a potential move, then perform 
+         * move */
+        move_performed = 0;
+        do {
+            potential_move = rand() % 4;
+
+            switch (potential_move) {
+                case LEFT:
+                    if (potential_moves[LEFT] == true) {
                         col--;
-                        performed_move = 1;
+                        move_performed = 1;
                     }
                     break;
-                case 1: // Down
-                    if (row + 1 < ROWS && grid[row+1][col] == EMPTY) {
+                case DOWN:
+                    if (potential_moves[DOWN] == true) {
                         row++;
-                        performed_move = 1;
+                        move_performed = 1;
                     }
                     break;
-                case 2: // Up
-                    if (row - 1 >= 0 && grid[row-1][col] == EMPTY) {
+                case UP:
+                    if (potential_moves[UP] == true) {
                         row--;
-                        performed_move = 1;
+                        move_performed = 1;
                     }
                     break;
-                case 3: // Right
-                    if (col + 1 < COLS && grid[row][col+1] == EMPTY) {
+                case RIGHT:
+                    if (potential_moves[RIGHT] == true) {
                         col++;
-                        performed_move = 1;
+                        move_performed = 1;
                     }
                     break;
             }
-            if (performed_move)
-                break;
-        }
+
+        } while (!move_performed);
     }
              
     /* Print the final grid state */
