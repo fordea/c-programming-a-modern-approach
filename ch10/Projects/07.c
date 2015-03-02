cd27f93b97cb26cdaf1aa5e38f1a79ee77b16fe0
Alex Forde <fordea90@gmail.com>
2015-03-02 22:40:19 +0000

    Modified some comments for clarity in Project 7, Chapter 10. Also changed some minor code snippets.

--- ch10/Projects/07.c
+++ ch10/Projects/07.c
@@ -2,36 +2,62 @@
 #include <ctype.h>
 
 #define MAX_DIGITS 10
+
 #define DIGIT_HEIGHT 3
 #define DIGIT_WIDTH 3
-#define DIGIT_SPACING 1
-#define MAX_DIGITS_SPACE MAX_DIGITS * (DIGIT_WIDTH + DIGIT_SPACING)
-#define TOTAL_SEGMENTS 7
+#define DIGIT_SPACING 4
+
+#define MAX_DIGITS_SPACE (MAX_DIGITS * (DIGIT_WIDTH + DIGIT_SPACING)) - DIGIT_SPACING
+#define MAX_SEGMENTS 7
 
 void clear_digits_array(void);
 void process_digit(int digit, int position);
 void print_digits_array(void);
 
-const int segments[10][7] = {{1, 1, 1, 1, 1, 1, 0}, /* 0 */
-/* Row 0:       0     */     {0, 1, 1, 0, 0, 0, 0}, /* 1 */
-/*             ---    */     {1, 1, 0, 1, 1, 0, 1}, /* 2 */
-/*            |   |   */     {1, 1, 1, 1, 0, 0, 1}, /* 3 */
-/* Row 1:   5 | 6 | 1 */     {0, 1, 1, 0, 0, 1, 1}, /* 4 */
-/*             ---    */     {1, 0, 1, 1, 0, 1, 1}, /* 5 */
-/*            |   |   */     {1, 0, 1, 1, 1, 1, 1}, /* 6 */
-/* Row 2:   4 | 3 | 2 */     {1, 1, 1, 0, 0, 0, 0}, /* 7 */
-/*             ---    */     {1, 1, 1, 1, 1, 1, 1}, /* 8 */
-/*                    */     {1, 1, 1, 1, 0, 1, 1}};/* 9 */
+/* Contains digit segments for all digits 0 through 9.
+ * Segments are 'off' if 0, and 'on' if 1.
+ * All digits can be represented using a maximum of 7 segments.
+ *
+ *     0
+ *    ___ 
+ * 5 |   | 1
+ *   |_6_|
+ * 4 |   | 2
+ *   |___|
+ *     3
+ */
+const int digit_segments[10][7] = {
+/*Segment: 0  1  2  3  4  5  6  | Digit | */
+          {1, 1, 1, 1, 1, 1, 0}, /* 0 */
+          {0, 1, 1, 0, 0, 0, 0}, /* 1 */
+          {1, 1, 0, 1, 1, 0, 1}, /* 2 */
+          {1, 1, 1, 1, 0, 0, 1}, /* 3 */
+          {0, 1, 1, 0, 0, 1, 1}, /* 4 */
+          {1, 0, 1, 1, 0, 1, 1}, /* 5 */
+          {1, 0, 1, 1, 1, 1, 1}, /* 6 */
+          {1, 1, 1, 0, 0, 0, 0}, /* 7 */
+          {1, 1, 1, 1, 1, 1, 1}, /* 8 */
+          {1, 1, 1, 1, 0, 1, 1}  /* 9 */
+};
 
-/* row, col position of the seven segments in a 3 by 3 grid 
- * positions 0,0 and 0,2 do not contain a segment */
-const int segment_rowcol[TOTAL_SEGMENTS][2] = { {0, 1},   /* 0 */
-                                                {1, 2},   /* 1 */
-                                                {2, 2},   /* 2 */
-                                                {2, 1},   /* 3 */
-                                                {2, 0},   /* 4 */
-                                                {1, 0},   /* 5 */
-                                                {1, 1} }; /* 6 */
+/* Row, col positions of the 7 segments in a 3 by 3 grid 
+ * positions 0,0 and 0,2 do not contain a segment 
+ *
+ *    012 
+ *  0  _
+ *  1 |_|
+ *  2 |_| 
+ */
+const int segment_grid_pos[MAX_SEGMENTS][2] = {
+/*  row  col | Segment | */
+    {0,   1},  /* 0 */
+    {1,   2},  /* 1 */
+    {2,   2},  /* 2 */
+    {2,   1},  /* 3 */
+    {2,   0},  /* 4 */
+    {1,   0},  /* 5 */
+    {1,   1}   /* 6 */
+};
 
 char digits[DIGIT_HEIGHT][MAX_DIGITS_SPACE];
 
@@ -42,17 +68,16 @@ int main(void)
 
     clear_digits_array();
     printf("Enter a number (up to 10 digits): ");
-    while ((ch = getchar()) != '\n' && position <= MAX_DIGITS_SPACE) {
+    while ((ch = getchar()) != '\n' && position < MAX_DIGITS_SPACE && isdigit(ch)) {
 
-        if (isdigit(ch)) {
-            process_digit(ch - '0', position);
-            position += DIGIT_WIDTH + DIGIT_SPACING;
-        }
+        process_digit(ch - '0', position);
+        position += DIGIT_WIDTH + DIGIT_SPACING;
     }
     print_digits_array();
 
 }
 
+/* Fill the digits array with empty spaces */
 void clear_digits_array(void)
 {
     int row, col;
@@ -62,21 +87,17 @@ void clear_digits_array(void)
     }
 }
 
-/* Process a single digit */
-void process_digit(int digit, int position)
+/* Construct a digit in the digits array at the starting col */
+void process_digit(int digit, int starting_col)
 {
     int segment, segment_row_pos, segment_col_pos;
 
-    for (segment = 0; segment < TOTAL_SEGMENTS; segment++) {
+    for (segment = 0; segment < MAX_SEGMENTS; segment++) {
 
-        if (segments[digit][segment]) {
-            segment_row_pos = segment_rowcol[segment][0];
-            segment_col_pos = position + segment_rowcol[segment][1];
-
-            if (segment % 3 == 0) //segments divisible by 3 are always '_'
-                digits[segment_row_pos][segment_col_pos] = '_';
-            else
-                digits[segment_row_pos][segment_col_pos] = '|';
+        if (digit_segments[digit][segment]) {
+            segment_row_pos = segment_grid_pos[segment][0];
+            segment_col_pos = segment_grid_pos[segment][1] + starting_col;
+            digits[segment_row_pos][segment_col_pos] = segment % 3 == 0 ? '_' : '|';
         }
     }
 }
