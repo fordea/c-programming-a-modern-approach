b42e3a1deeefac521f7a8401273be7d89fbdde03
Alex Forde <fordea90@gmail.com>
2015-03-06 17:14:51 +0000

    Completed Project 7 in Chapter 12.

---
+++ ch12/Projects/07.c
@@ -0,0 +1,46 @@
+/*********************************************************
+ * From C PROGRAMMING: A MODERN APPROACH, Second Edition *
+ * By K. N. King                                         *
+ * Copyright (c) 2008, 1996 W. W. Norton & Company, Inc. *
+ * All rights reserved.                                  *
+ * This program may be freely distributed for class use, *
+ * provided that this copyright notice is retained.      *
+ *********************************************************/
+
+/* maxmin.c (Chapter 11, page 250) */
+/* Finds the largest and smallest elements in an array */
+
+#include <stdio.h>
+
+#define N 10
+
+void max_min(int a[], int n, int *max, int *min);
+
+int main(void)
+{
+  int b[N], i, big, small;
+
+  printf("Enter %d numbers: ", N);
+  for (i = 0; i < N; i++)
+    scanf("%d", &b[i]);
+
+  max_min(b, N, &big, &small);
+
+  printf("Largest: %d\n", big);
+  printf("Smallest: %d\n", small);
+
+  return 0;
+}
+
+void max_min(int a[], int n, int *max, int *min)
+{
+  int *p;
+
+  *max = *min = a[0];
+  for (p = a + 1; p < a + n; p++) {
+    if (*p > *max)
+      *max = *p;
+    else if (*p < *min)
+      *min = *p;
+  }
+}
