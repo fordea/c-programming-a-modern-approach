7672ed7a3c0c6d6905cf4a82ad2ddf564acf616b
Alex Forde <fordea90@gmail.com>
2015-03-10 21:34:35 +0000

    Completed Project 3 in Chapter 13.

---
+++ ch13/Projects/03.c
@@ -0,0 +1,48 @@
+/*********************************************************
+ * From C PROGRAMMING: A MODERN APPROACH, Second Edition *
+ * By K. N. King                                         *
+ * Copyright (c) 2008, 1996 W. W. Norton & Company, Inc. *
+ * All rights reserved.                                  *
+ * This program may be freely distributed for class use, *
+ * provided that this copyright notice is retained.      *
+ *********************************************************/
+
+/* deal.c (Chapter 8, page 173) */
+/* Deals a random hand of cards */
+
+#include <stdbool.h>   /* C99 only */
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define NUM_SUITS 4
+#define NUM_RANKS 13
+
+int main(void)
+{
+  bool in_hand[NUM_SUITS][NUM_RANKS] = {false};
+  int num_cards, rank, suit;
+  const char *rank_code[] = {"Two", "Three", "Four", "Five", "Six",
+                             "Seven", "Eight", "Nine", "Ten", "Jack",
+                             "Queen", "King", "Ace"};
+
+  const char *suit_code[] = {"Clubs", "Diamonds", "Hearts", "Spades"};
+
+  srand((unsigned) time(NULL));
+
+  printf("Enter number of cards in hand: ");
+  scanf("%d", &num_cards);
+
+  printf("Your hand:");
+  while (num_cards > 0) {
+    suit = rand() % NUM_SUITS;     /* picks a random suit */
+    rank = rand() % NUM_RANKS;     /* picks a random rank */
+    if (!in_hand[suit][rank]) {
+      in_hand[suit][rank] = true;
+      num_cards--;
+      printf("\n%s of %s", *(rank_code + rank), *(suit_code + suit));
+    }
+  }
+
+  return 0;
+}
