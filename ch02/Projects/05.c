53dc9f90164e2d01b9d23787e31562214ee9d3d6
Alex Forde <fordea90@gmail.com>
2015-03-18 01:30:31 +0000

    Fixed an error in Chapter 2, Project 5. Polynomial was calculated incorrectly as some operators were wrong type.

--- ch02/Projects/05.c
+++ ch02/Projects/05.c
@@ -8,9 +8,9 @@ int main(void)
     scanf("%d", &x);
     printf("%d", (3 * (x * x * x * x * x))
                  + (2 * (x * x * x * x))
-                 + (5 * (x * x * x))
-                 + (x * x)
-                 - (7 * x)
+                 - (5 * (x * x * x))
+                 - (x * x)
+                 + (7 * x)
                  - 6);
 
     return 0;
