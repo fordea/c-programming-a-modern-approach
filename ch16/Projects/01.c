5ad6bebcec5f621a04c5af7ec92f86f2fdf74a51
Alex Forde <fordea90@gmail.com>
2015-03-22 01:10:32 +0000

    Completed Project 1 in Chapter 16.

---
+++ ch16/Projects/01.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+struct dialing_code {
+    char *country;
+    int code;
+};
+
+const struct dialing_code country_codes[] =
+    {{"Argentina",            54}, {"Bangladesh",     889},
+     {"Brazil",               55}, {"Burma (Myanmar)", 95},
+     {"China",                86}, {"Colombia",        57},
+     {"Congo, Dem. Rep. of", 243}, {"Egypt",           20},
+     {"Ethiopia",            251}, {"France",          33},
+     {"Germany",              49}, {"India",           91},
+     {"Indonesia",            62}, {"Iran",            98},
+     {"Italy",                39}, {"Japan",           81},
+     {"Mexico",               52}, {"Nigeria",        234},
+     {"Pakistan",             92}, {"Philippines",     63},
+     {"Poland",               48}, {"Russia",           7},
+     {"South Africa",         27}, {"South Korea",     82},
+     {"Spain",                34}, {"Sudan",          249},
+     {"Thailand",             66}, {"Turkey",          90},
+     {"Ukraine",             380}, {"United Kingdom",  44},
+     {"United States",         1}, {"Vietnam",         84}};
+
+int main(void)
+{
+    int i, code;
+    printf("Enter a country code: ");
+    scanf("%d", &code);
+
+    for (i = 0; i < sizeof(country_codes) / sizeof(country_codes[0]); i++) {
+        if (country_codes[i].code == code) {
+            printf("Country code %d is for %s\n", code, country_codes[i].country);
+            return 0;
+        }
+    }
+    printf("Error: Country code %d doesn't match any country\n", code);
+
+    return 0;
+}
