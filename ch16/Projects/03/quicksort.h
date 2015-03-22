11a08a92aff9adf1ff02fe5ca4735e119294b71f
Alex Forde <fordea90@gmail.com>
2015-03-22 12:57:54 +0000

    Completed Project 3 in Chapter 16.

---
+++ ch16/Projects/03/quicksort.h
@@ -0,0 +1,8 @@
+#ifndef QUICKSORT_H
+#define QUICKSORT_H
+
+#include "inventory.h"
+/* Sorts a struct part array a using the quicksort algorithm */
+void quicksort(struct part a[], int low, int high);
+
+#endif
