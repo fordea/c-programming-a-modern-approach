982338166cfbab7f2aefa6837a41fd028892798d
Alex Forde <fordea90@gmail.com>
2015-03-24 00:27:03 +0000

    Updated linked list with clear list function.

--- ch17/Exercises/linked_list.c
+++ ch17/Exercises/linked_list.c
@@ -10,6 +10,7 @@ void *add_to_list(struct node **pp, int n);
 void *delete_from_list(struct node **pp, int n);
 struct node *search_list(struct node *p, int n);
 int list_size(struct node *p);
+void clear_list(struct node **pp);
 
 
 int main(void)
@@ -18,12 +19,18 @@ int main(void)
 
     add_to_list(&head, 25);
     add_to_list(&head, 13);
+    add_to_list(&head, 38);
+    add_to_list(&head, 23);
+    add_to_list(&head, 30);
     printf("Number of nodes in list: %d\n", list_size(head));
 
     printf("%s\n", search_list(head, 12) ? "Found Node." : "Node not found.");
     printf("%s\n", search_list(head, 25) ? "Found Node." : "Node not found.");
 
-    delete_from_list(&head, 25);
+    delete_from_list(&head, 38);
+    printf("Number of nodes in list: %d\n", list_size(head));
+
+    clear_list(&head);
     printf("Number of nodes in list: %d\n", list_size(head));
 
     return 0;
@@ -85,4 +92,14 @@ int list_size(struct node *p)
         p = p->next;
     }
     return count;
+}
+void clear_list(struct node **pp)
+{
+    struct node *next_node;
+
+    while (*pp != NULL) {
+        next_node = (*pp)->next;
+        free(*pp);
+        *pp = next_node;
+    }
 }
