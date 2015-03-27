807201c144431ee613d5ef3ef67ae2818c2c63cd
Alex Forde <fordea90@gmail.com>
2015-03-27 17:15:27 +0000

    Added some functions.

--- ch17/Projects/05.c
+++ ch17/Projects/05.c
@@ -5,45 +5,54 @@
 #define MAX_WORD_LEN 20
 
 int compare_words(const void *w1, const void *w2);
+void *my_malloc(char *p, int bytes);
+void sort_and_print(char **pp, int n);
 
 int main(void)
 {
-    char **words;
-    char *word;
-    int i = 0, num_words = 0;
+    char **words, *word;
+    int i, size = 1, num_words = 0;
 
-    /* Allocate enough space for a single char pointer in words array */
-    if ((words = malloc (sizeof(char *))) == NULL) {
-        printf("Error, malloc failed.\n");
-        exit(EXIT_FAILURE);
-    }
+    words = my_malloc(*words, sizeof(char *));
 
-    for (i = 0, num_words = 0 ; ; i++, num_words++) {
+    for (i = 0; ;i++) {
 
-        /* Allocate enough space for a 20 char word */
-        if ((word = malloc(MAX_WORD_LEN + 1)) == NULL) {
-            printf("Error, malloc failed.\n");
-            exit(EXIT_FAILURE);
-        }
-
+        word = my_malloc(word, MAX_WORD_LEN + 1);
         printf("Enter a word: ");
         fgets(word, MAX_WORD_LEN + 1, stdin);
-        word[strlen(word) - 1] = '\0'; /* Remove new line char */
+        if (word[strlen(word) - 1] == '\n')
+            word[strlen(word) - 1] = '\0'; /* Remove new line char */
 
-        if (word[0] == '\0') /* Break when empty input */
+        if (word[0] == '\0')
             break;
 
         *(words + i) = word;
+        num_words++;
 
-        /* Increase the size of words to accomodate one more char * pointer */
-        words = realloc(words, sizeof(char *) * (num_words + 2));
+        if (size == num_words)
+            words = realloc(words, sizeof(char *) * (size *= 2));
     }
-    qsort(words, num_words, sizeof(char *), compare_words);
 
+    sort_and_print(words, num_words);
+}
+
+void *my_malloc(char *p, int bytes)
+{
+    if ((p = malloc(bytes)) == NULL) {
+        printf("Error, malloc failed\n");
+        exit(EXIT_FAILURE);
+    }
+}
+
+void sort_and_print(char **pp, int n)
+{
+    qsort(pp, n, sizeof(char *), compare_words);
     printf("\nWords sorted\n");
-    for (i = 0; i < num_words; i++) {
-        printf("%s\n", *(words + i));
+    int i;
+    for (i = 0; i < n; i++) {
+        printf("%s ", *(pp + i));
     }
+    printf("\n");
 }
 
 int compare_words(const void *w1, const void *w2)
