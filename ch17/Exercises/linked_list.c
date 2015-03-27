54ab5ae8af19a1d700205ef5c48d5f67ddd8aa9a
Alex Forde <fordea90@gmail.com>
2015-03-27 14:53:51 +0000

    Update functions that use a pointer to pointer, removed unnecessary entry pointer.

--- ch17/Exercises/linked_list.c
+++ ch17/Exercises/linked_list.c
@@ -14,9 +14,11 @@ void clear_list(void);
 int list_size(void);
 void print_node_values(void);
 
+static int size = 0;
 
 int main(void)
 {
+    printf("Normal linked list\n");
     add_to_list(1);
     add_to_list(2);
     add_to_list(3);
@@ -26,19 +28,22 @@ int main(void)
     clear_list();
     printf("\n");
 
-    add_to_ordered_list(1);
-    add_to_ordered_list(2);
-    add_to_ordered_list(3);
-    add_to_ordered_list(4);
-    add_to_ordered_list(5);
+    printf("Ordered linked list\n");
+    add_to_ordered_list(24);
+    add_to_ordered_list(83);
+    add_to_ordered_list(94);
+    add_to_ordered_list(123);
+    add_to_ordered_list(389);
+    add_to_ordered_list(12);
+    add_to_ordered_list(84);
     print_node_values();
-    delete_from_list(4);
+    delete_from_list(12);
     printf("\n");
     print_node_values();
     printf("\n");
 
-    printf("%s\n", search_list(2) ? "Found Node." : "Node not found.");
-    printf("%s\n", search_list(4) ? "Found Node." : "Node not found.");
+    printf("%s\n", search_list(389) ? "Found Node." : "Node not found.");
+    printf("%s\n\n", search_list(4) ? "Found Node." : "Node not found.");
 
     printf("Number of nodes in list: %d\n", list_size());
 
@@ -61,6 +66,7 @@ void *add_to_list(int n)
     new_node->value = n;
     new_node->next = head;
     head = new_node;
+    size++;
 }
 
 /* Delete a node from the linked list. Argument pp is a pointer to a pointer
@@ -68,15 +74,16 @@ void *add_to_list(int n)
 void *delete_from_list(int n)
 {
     struct node **pp = &head;
-    struct node *entry = head;
+    struct node *temp;
 
-    while (entry) {
-        if (entry->value == n) {
-            *pp = entry->next;
-            free(entry);
+    while (*pp) {
+        if ((*pp)->value == n) {
+            temp = *pp;
+            *pp = (*pp)->next;
+            free(temp);
+            size--;
         }
-        pp = &entry->next;
-        entry = entry->next;
+        pp = &(*pp)->next;
     }
 }
 
@@ -96,13 +103,7 @@ struct node *search_list(int n)
  * a pointer to the first node in the list */
 int list_size(void)
 {
-    int count = 0;
-    struct node *entry;
-
-    for (entry = head; entry; entry = entry->next, count++)
-        ; /* Empty loop body */
-
-    return count;
+    return size;
 }
 
 /* Frees all memory allocated to nodes in the linked list */
@@ -117,20 +118,19 @@ void clear_list(void)
         head = head->next;
         free(temp);
     }
+    size = 0;
 }
 
 /* Inserts a node into list in numerical order by value */
 void *add_to_ordered_list(int n)
 {
     struct node **pp = &head;
-    struct node *entry = head;
 
-    while (entry) {
-        if (entry->value >= n) {
+    while (*pp) {
+        if ((*pp)->value >= n) {
             break;
         }
-        pp = &entry->next;
-        entry = entry->next;
+        pp = &(*pp)->next;
     }
 
     struct node *new_node;
@@ -138,9 +138,10 @@ void *add_to_ordered_list(int n)
         printf("Error, malloc in insert_into_ordered_list\n");
         exit(EXIT_FAILURE);
     }
-    new_node->next = entry;
+    new_node->next = *pp;
     new_node->value = n;
     *pp = new_node;
+    size++;
 }
 
 /* Prints all the nodes and their values in the list */
