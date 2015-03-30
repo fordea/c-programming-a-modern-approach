d1d481e155596fea7995fdef8b4297002273f6c1
Alex Forde <fordea90@gmail.com>
2015-03-30 20:20:01 +0100

    Completed Exercise 1 in Chapter 19.

---
+++ ch19/Exercises/01/queue.h
@@ -0,0 +1,23 @@
+#ifndef QUEUE_H
+#define QUEUE_H
+
+#include <stdbool.h>
+
+typedef struct queue_type *Queue;
+
+/* Appends Item i to the end of the queue */
+void enqueue(Item i);
+
+/* Removes and returns Item i from the front of the queue */
+Item dequeue(void);
+
+/* Returns but doesn't remove Item i from the front of the queue */
+Item get_first(void);
+
+/* Returns but doesn't remove Item i from the end of the queue */
+Item get_last(void);
+
+/* Returns true if the queue is empty */
+bool is_empty(void);
+
+#endif
