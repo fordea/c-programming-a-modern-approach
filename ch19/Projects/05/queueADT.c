56001f7bcb0a2fb2cf15373d7c61efb8d30ec741
Alex Forde <fordea90@gmail.com>
2015-04-02 22:41:07 +0100

    Completed Project 5 in Chapter 19.

---
+++ ch19/Projects/05/queueADT.c
@@ -0,0 +1,93 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "queue.h"
+
+#define QUEUE_MAX 100
+
+static void terminate(char *message);
+
+struct queue_type {
+    Item contents[QUEUE_MAX];
+    int first;
+    int next;
+    int len;
+};
+
+Queue create(void)
+{
+    Queue q;
+    if ((q = malloc(sizeof(struct queue_type))) == NULL)
+    {
+        printf("Error, malloc failed in create()\n");
+        exit(EXIT_FAILURE);
+    }
+    q->first = 0;
+    q->next = 0;
+    q->len = 0;
+    return q;
+}
+
+void destroy(Queue q)
+{
+    empty_queue(q);
+    free(q);
+}
+
+void enqueue(Queue q, Item i)
+{
+    if (!is_full(q))
+    {
+        q->contents[q->next++] = i;
+        q->len++;
+    }
+}
+
+Item dequeue(Queue q)
+{
+    if (!is_empty(q))
+    {
+        Item i = q->contents[q->first++];
+        q->len--;
+        return i;
+    }
+}
+
+Item peek_front(Queue q)
+{
+    if (!is_empty(q))
+    {
+        return q->contents[q->first];
+    }
+}
+
+Item peek_end(Queue q)
+{
+    if (!is_empty(q))
+    {
+        return q->contents[q->next - 1];
+    }
+}
+
+bool is_empty(Queue q)
+{
+    return q->len == 0;
+}
+
+bool is_full(Queue q)
+{
+    return q->len == QUEUE_MAX;
+}
+
+void empty_queue(Queue q)
+{
+    while (!is_empty(q))
+    {
+        dequeue(q);
+    }
+}
+
+static void terminate(char *message)
+{
+    printf("%s\n", message);
+    exit(EXIT_FAILURE);
+}
