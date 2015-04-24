1bcd4edd5ccbc9e68a0a433950444e4e76e97a5a
Alex Forde <fordea90@gmail.com>
2015-04-24 19:00:10 +0100

    Fixed error in CHECK macro where lower bound check was 1, changed to 0.

--- ch14/Exercises/09.c
+++ ch14/Exercises/09.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-#define CHECK(x,y,n) (((x) >= 1 && (x) <= ((n)-1)) && ((y) >= 1 && (y) <= ((n)-1)))
+#define CHECK(x,y,n) (((x) >= 0 && (x) <= ((n)-1)) && ((y) >= 0 && (y) <= ((n)-1)))
 #define MEDIAN(x,y,z) (((x) - (y)) * ((z) - (x)) >= 0 ? x : ((y) - (x)) * ((z) - (y)) >= 0 ? y : z)
 #define POLYNOMIAL(x) ( (3 * ((x) * (x) * (x) * (x) * (x)))         \
                        +(2 * ((x) * (x) * (x) * (x)))               \
@@ -10,7 +10,7 @@
 int main(void)
 {
     int x, y, n;
-    x = 5; y = 10, n = 7;
+    x = 5; y = 10, n = 11;
 
     printf("CHECK(%d,%d,%d) = %d\n", x, y, n, CHECK(x,y,n));
     printf("MEDIAN(%d,%d,%d) = %d\n", x, y, n, MEDIAN(x,y,n));
