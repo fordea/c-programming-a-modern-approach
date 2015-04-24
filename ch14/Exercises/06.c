be32e9726447dc09bceae9b36572cac52fe8f520
Alex Forde <fordea90@gmail.com>
2015-04-24 18:43:38 +0100

    Added part (b) to Exercise 6.

--- ch14/Exercises/06.c
+++ ch14/Exercises/06.c
@@ -2,8 +2,11 @@
 #include <math.h>
 
 #define DISP(f,x) (printf("%s(%g) = %g\n", (#f), (x), (f(x))))
+#define DISP2(f,x,y) (printf("%s(%g,%g) = %g\n", (#f), (x), (y), (f((x),(y)))))
 
 int main(void)
 {
-    DISP(sqrt, 3.0);
+    DISP(sqrt, 1024.0);
+    DISP2(pow, 32.0, 2.0);
+    return 0;
 }
