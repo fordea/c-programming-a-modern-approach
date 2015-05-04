2fa7f34285b4f34242897a0946c222a5a108b566
Alex Forde <fordea90@gmail.com>
2015-05-04 23:42:29 +0100

    Completed part (b) in Project 7 in Chapter 22.

--- ch22/Projects/07.c
+++ ch22/Projects/07.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 
 typedef unsigned char BYTE;
 
 void compress_file(char *filename);
 void uncompress_file(char *filename);
+bool is_compressed_file(char *filename);
 
 int main(int argc, char *argv[])
 {
@@ -14,7 +16,22 @@ int main(int argc, char *argv[])
         exit(EXIT_FAILURE);
     }
 
-    compress_file(argv[1]);
+    if (is_compressed_file(argv[1])) {
+        uncompress_file(argv[1]);
+    }
+    else {
+        compress_file(argv[1]);
+    }
+}
+
+bool is_compressed_file(char *filename) {
+    char compression_ext[] = ".rle";
+    char *p = filename;
+    while (*p++);
+    while (p >= filename && *p != '.') {
+        p--;
+    }
+    return strcmp(p, compression_ext) == 0;
 }
 
 void compress_file(char *filename) {
@@ -51,6 +68,43 @@ void compress_file(char *filename) {
         fwrite(&sequence, sizeof(BYTE), 1, fpout);
         fwrite(&origbyte, sizeof(BYTE), 1, fpout);
         fseek(fpin, pos + (sequence - 1), SEEK_SET);
+    }
+    fclose(fpin);
+    fclose(fpout);
+}
+
+void uncompress_file(char *filename)
+{
+    FILE *fpin;
+    FILE *fpout;
+
+    if ((fpin = fopen(filename, "rb")) == NULL) {
+        printf("Error, cannot open file %s.\n", filename);
+        exit(EXIT_FAILURE);
+    }
+
+    char *outfilename = malloc(strlen(filename) - 3);
+    strncpy(outfilename, filename, strlen(filename) - 4);
+
+    if ((fpout = fopen(outfilename, "wb")) == NULL) {
+        printf("Error, cannot create file %s.\n", outfilename);
+        exit(EXIT_FAILURE);
+    }
+    free(outfilename);
+
+    BYTE b;
+    BYTE repetitions;
+    int i = 0;
+    while (fread(&b, sizeof(BYTE), 1, fpin) > 0) {
+        if (i % 2 == 0) {
+            repetitions = b;
+        }
+        else {
+            for (int j = 0; j < repetitions; ++j) {
+                fwrite(&b, sizeof(BYTE), 1, fpout);
+            }
+        }
+        ++i;
     }
     fclose(fpin);
     fclose(fpout);
