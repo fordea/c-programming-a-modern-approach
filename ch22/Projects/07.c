4d019d6af07ef5980943bd63ea37196e7da14773
Alex Forde <fordea90@gmail.com>
2015-05-04 20:46:39 +0100

    Complete part (a) of Project 7 in Chapter 22.

---
+++ ch22/Projects/07.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef unsigned char BYTE;
+
+void compress_file(char *filename);
+void uncompress_file(char *filename);
+
+int main(int argc, char *argv[])
+{
+    if (argc != 2) {
+        printf("Usage: programname inputfile.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    compress_file(argv[1]);
+}
+
+void compress_file(char *filename) {
+
+    FILE *fpin;
+    FILE *fpout;
+
+    if ((fpin = fopen(filename, "rb")) == NULL) {
+        printf("Error, cannot open file %s.\n", filename);
+        exit(EXIT_FAILURE);
+    }
+
+    char *outfilename = malloc(strlen(filename) + 5);
+    strcpy(outfilename, filename);
+    strcat(outfilename, ".rle");
+
+    if ((fpout = fopen(outfilename, "wb")) == NULL) {
+        printf("Error, cannot create file %s.\n", outfilename);
+        exit(EXIT_FAILURE);
+    }
+    free(outfilename);
+
+    BYTE origbyte;
+    BYTE cmpbyte;
+    int pos;
+    BYTE sequence; //Allows sequences upto 255
+    while (fread(&origbyte, sizeof(BYTE), 1, fpin) > 0) {
+        sequence = 1;
+        pos = ftell(fpin);
+        while (fread(&cmpbyte, sizeof(BYTE), 1, fpin) > 0
+                && cmpbyte == origbyte) {
+            ++sequence;
+        }
+        fwrite(&sequence, sizeof(BYTE), 1, fpout);
+        fwrite(&origbyte, sizeof(BYTE), 1, fpout);
+        fseek(fpin, pos + (sequence - 1), SEEK_SET);
+    }
+    fclose(fpin);
+    fclose(fpout);
+}
