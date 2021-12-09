15ad1a7c2117bc8d0fc64ca4033be4bf37f5fab0
abe-101 <slimhacker@pm.me>
2021-12-09 13:07:55 -0500

    change def to extern

--- ch16/Projects/02/inventory.h
+++ ch16/Projects/02/inventory.h
@@ -4,10 +4,10 @@
 #define NAME_LEN 25
 #define MAX_PARTS 100
 
-struct part {
+extern struct part {
   int number;
   char name[NAME_LEN+1];
   int on_hand;
-} inventory[MAX_PARTS];
+} inventory[];
 
 #endif
